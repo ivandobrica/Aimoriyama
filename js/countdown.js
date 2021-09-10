7d8362d722d98316fba1ba92c5a9dc2b7b5f2ff7
ivandobrica <dobrica.ivan@hotmail.com>
2021-09-10 22:21:48 +0200

    Added images alt text, shop countdown, button aria

---
+++ js/countdown.js
@@ -0,0 +1,15 @@
+const second = 1000,
+    minute = second * 60,
+    hour = minute * 60,
+    day = hour * 24;
+  
+let birthday = "Aug 23, 2022 00:00:00",
+countDown = new Date(birthday).getTime(),
+x = setInterval(function() {    
+    let now = new Date().getTime(),
+    distance = countDown - now;
+    document.querySelector(".days").innerText = Math.floor(distance / (day)),
+    document.querySelector(".hours").innerText = Math.floor((distance % (day)) / (hour)),
+    document.querySelector(".minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+    document.querySelector(".seconds").innerText = Math.floor((distance % (minute)) / second);
+}, 0)
